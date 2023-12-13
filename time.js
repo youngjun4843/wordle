287576873765812ea25561f0b7e2fa98c1acfca1
youngjun4843 <152593642+youngjun4843@users.noreply.github.com>
2023-12-13 20:01:20 +0900

    [워들] HTML, Css 구현

---
+++ time.js
@@ -0,0 +1,23 @@
+const 시작_시간 = new Date();
+
+function setTime() {
+  const 현재_시간 = new Date();
+  const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+  const 분 = 흐른_시간.getMinutes().toString().padStart(2, "0");
+  const 초 = 흐른_시간.getSeconds().toString().padStart(2, "0");
+  const timeH1 = document.querySelector(".time");
+  timeH1.innerText = `time : ${분}:${초}`;
+}
+
+setInterval(setTime, 1000);
+
+// const 시작_시간 = new Date();
+
+// function setTime() {
+//   const 현재_시간 = new Date();
+//   const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+//   const 분 = 흐른_시간.getMinutes().toString().padStart(2, "0");
+//   const 초 = 흐른_시간.getSeconds().toString().padStart(2, "0");
+//   const timeH1 = document.querySelector(".time");
+//   timeH1.innerText = `time: ${분}:${초}`;
+// }
